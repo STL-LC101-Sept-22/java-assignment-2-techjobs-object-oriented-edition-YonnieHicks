2ca8261c90ab990f97f7668e0e36e2001b80a9da
YonnieHicks <85805865+YonnieHicks@users.noreply.github.com>
2023-02-23 17:39:51 -0600

    Update JobTest.java

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -6,6 +6,15 @@ import static org.junit.Assert.*;
 
 import org.launchcode.techjobs.oo.*;
 
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Created by LaunchCode
+ */
+@RunWith(JUnit4.class)
+
 public class JobTest {
 
     @Test
