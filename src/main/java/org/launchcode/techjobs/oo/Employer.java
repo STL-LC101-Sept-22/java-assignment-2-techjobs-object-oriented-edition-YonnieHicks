19ee8b8e68adda7f0d9de783f1c33450ed9b1315
YonnieHicks <85805865+YonnieHicks@users.noreply.github.com>
2023-03-02 12:58:00 -0600

    Removed empty constructor from Employer class

--- src/main/java/org/launchcode/techjobs/oo/Employer.java
+++ src/main/java/org/launchcode/techjobs/oo/Employer.java
@@ -2,10 +2,6 @@ package org.launchcode.techjobs.oo;
 
 public class Employer extends JobField {
 
-    public Employer() {
-        super();
-    }
-
     public Employer(String value) {
         super(value);
     }
